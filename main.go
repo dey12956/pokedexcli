37f9edae4a4feb3ac27e37567e7ec5bd7241d5ee
dey <dey12956@gmail.com>
2026-01-22 13:19:51 +0800

    build commands help and exit

--- main.go
+++ main.go
@@ -11,10 +11,56 @@ func main() {
 
 	for {
 		fmt.Print("Pokedex > ")
-		if !scanner.Scan() {break}
+		register()
+		if !scanner.Scan() { break }
 		input := scanner.Text()
 		words := cleanInput(input)
 		if len(words) == 0 { continue }
-		fmt.Printf("Your command was: %s\n", words[0])
+		if command, ok := commands[words[0]]; ok{
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
 	}
+}
+
+type cliCommand struct {
+	name string
+	description string
+	callback func() error
+}
+
+var commands map[string]cliCommand
+
+func register() {
+	commands = map[string]cliCommand{
+		"exit": {
+			name: "exit",
+			description: "Exit the Pokedex",
+			callback: commandExit,
+		},
+		"help": {
+			name: "help",
+			description: "Displays a help message",
+			callback: commandHelp,
+		},
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	for name, command := range commands {
+		fmt.Printf("%s: %s\n", name, command.description)
+	}
+	return nil
 }
